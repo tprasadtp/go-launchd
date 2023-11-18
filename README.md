0c2a3d4434b88170d181a2e58e895ffd27e94ff6
Prasad Tengse <tprasadtp@users.noreply.github.com>
2023-11-19 00:18:05 +0100

    docs: add examples

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# Experimental
+# go-launchd
 
-[![Go Reference](https://pkg.go.dev/badge/github.com/tprasadtp/pkg.svg)](https://pkg.go.dev/github.com/tprasadtp/pkg)
-![GitHub go.mod Go version](https://img.shields.io/github/go-mod/go-version/tprasadtp/pkg?label=go&logo=go&logoColor=white)
-[![test](https://github.com/tprasadtp/pkg/actions/workflows/test.yml/badge.svg)](https://github.com/tprasadtp/pkg/actions/workflows/test.yml)
-![GitHub](https://img.shields.io/github/license/tprasadtp/pkg)
-![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/tprasadtp/pkg?color=7f50a6&label=release&logo=semver&sort=semver)
+[![Go Reference](https://pkg.go.dev/badge/github.com/tprasadtp/go-launchd.svg)](https://pkg.go.dev/github.com/tprasadtp/go-launchd)
+![GitHub go.mod Go version](https://img.shields.io/github/go-mod/go-version/tprasadtp/go-launchd?label=go&logo=go&logoColor=white)
+[![test](https://github.com/tprasadtp/go-launchd/actions/workflows/test.yml/badge.svg)](https://github.com/tprasadtp/go-launchd/actions/workflows/test.yml)
+[![GitHub](https://img.shields.io/github/license/tprasadtp/go-launchd)](https://github.com/tprasadtp/go-launchd/blob/master/LICENSE)
+[![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/tprasadtp/go-launchd?color=7f50a6&label=release&logo=semver&sort=semver)](https://github.com/tprasadtp/go-launchd/releases)
 
-- This repository makes no compatibility guarantees.
-- Pin deps to commits hashes!
-- Packages in this repository may be promoted/moved to `alpha` level
-under `github.com/tprasadtp/x/<module>` Unlike this repository, __some__
-effort is made to ensure compatibility and provides semver tagged releases.
-- Packages in this repository may also be moved to their own repository when they
-are considered `beta` stable.
+> **Warning**
+>
+> This module make use of syscall/unsafe package and thus may break between go releases
+> and between macOS versions.
+
+## Socket Activation
+
+Supports [Launchd Socket Activation](https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket) _without_ using cgo. See [docs](https://pkg.go.dev/github.com/tprasadtp/go-launchd) for more info and examples.
---
+++ activate_example_test.go
@@ -0,0 +1,99 @@
+// SPDX-FileCopyrightText: Copyright 2023 Prasad Tengse
+// SPDX-License-Identifier: MIT
+
+//go:build darwin && !ios
+
+package launchd_test
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/tprasadtp/go-launchd"
+)
+
+func ExampleListenersWithName() {
+	listeners, err := launchd.ListenersWithName("socket-name-as-in-plist")
+	if err != nil {
+		slog.Error("Error getting socket activated listeners", "err", err)
+		// Handle error and close any active listeners.
+		for _, item := range listeners {
+			item.Close()
+		}
+		os.Exit(1)
+	}
+
+	// A simple HTTP handler. Replace this with your actual implementation.
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello From Socket Activated Server\n"))
+		slog.Info("Request received",
+			"client", r.RemoteAddr,
+			"method", r.Method,
+			"url", r.URL)
+	})
+
+	// Make servers stoppable with ctrl+x or SIGTERM.
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	// WaitGroup to wait exiting multiple listeners.
+	var wg sync.WaitGroup
+
+	// Because there may be multiple listeners, we need to have as many servers as listeners.
+	servers := make([]*http.Server, 0, len(listeners))
+	for range listeners {
+		servers = append(servers, &http.Server{
+			Handler:           handler,
+			ReadHeaderTimeout: time.Second * 30,
+			BaseContext: func(_ net.Listener) context.Context {
+				return ctx
+			},
+		})
+	}
+
+	for i := 0; i < len(listeners); i++ {
+		// Run servers in background
+		wg.Add(1)
+		go func(s *http.Server, l net.Listener) {
+			defer wg.Done()
+			slog.Info("Starting server", "address", l.Addr())
+			if err := s.Serve(l); !errors.Is(err, http.ErrServerClosed) {
+				slog.Error("Error", "address", l.Addr(), "err", err)
+				cancel()
+			}
+		}(servers[i], listeners[i])
+
+		// Wait for context to cancel and stop server.
+		wg.Add(1)
+		go func(s *http.Server, l net.Listener) {
+			defer wg.Done()
+			var err error
+			//nolint:gosimple // https://github.com/dominikh/go-tools/issues/503
+			for {
+				select {
+				case <-ctx.Done():
+					slog.Info("Stopping server", "address", l.Addr())
+					// In production do it with timeout.
+					err = s.Shutdown(context.Background())
+					if err != nil && !errors.Is(err, http.ErrServerClosed) {
+						slog.Error("Failed to shutdown server",
+							"err", err, "address", l.Addr())
+					}
+					return
+				}
+			}
+		}(servers[i], listeners[i])
+	}
+
+	// Wait for all servers to exit.
+	wg.Wait()
+	slog.Info("Server(s) stopped")
+}
