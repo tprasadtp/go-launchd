e9af34d224f030d59a2f4d5f5c6373108cd50260
Prasad Tengse <tprasadtp@users.noreply.github.com>
2023-11-19 22:30:09 +0100

    api: provide method to get files

--- README.md
+++ README.md
@@ -6,14 +6,36 @@
 [![GitHub](https://img.shields.io/github/license/tprasadtp/go-launchd)](https://github.com/tprasadtp/go-launchd/blob/master/LICENSE)
 [![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/tprasadtp/go-launchd?color=7f50a6&label=release&logo=semver&sort=semver)](https://github.com/tprasadtp/go-launchd/releases)
 
-> **Warning**
->
-> This module makes use of syscall and unsafe packages thus may break between go-releases
-> and between macOS versions.
 
 ## Socket Activation
 
-Supports [Launchd Socket Activation](https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket) _without_ using cgo(`CGO_ENABLED=0`), by making calls to
-libc directly. This makes it simple to cross compile from your Linux/Windows CI machines.
+- Supports [Launchd Socket Activation][] __without using [cgo][]__(`CGO_ENABLED=0`).
+- This makes it simple to cross compile from your Linux/Windows CI machines.
+- Both UDP and TCP sockets are supported.
+- Supports both IPv4 and IPv6 and IPv4v6(single socket for both IPv4 and IPV6)
+
+## How it works
+
+This uses a similar technique as the [crypto/x509 package](https://go-review.googlesource.com/c/go/+/232397) via the `go:cgo_import_dynamic` directive.
+
+As this implementation depends on linker directives which is not part of go spec,
+and makes use of [syscall] and [unsafe] packages it may may break between go-releases and
+between macOS versions.
+
+## Usage
 
 See [API docs](https://pkg.go.dev/github.com/tprasadtp/go-launchd) for more info and examples.
+
+
+## Testing
+
+Testing requires macOS and go toolchain 1.21 or later.
+
+```
+go test -v ./...
+```
+
+[syscall]: https://pkg.go.dev/syscall
+[unsafe]: https://pkg.go.dev/unsafe
+[cgo]: https://pkg.go.dev/cmd/cgo
+[Launchd Socket Activation]: https://developer.apple.com/documentation/xpc1505523-launch_activate_socket
--- activate.go
+++ activate.go
@@ -14,14 +14,11 @@ import (
 // TCPListeners returns slice of [net.Listener] for specified TCP socket,
 // as mentioned in launchd plist file.
 //
-// This does not make use of cgo and makes calls to using libc directly.
-// This is obviously not supported on iOS.
-//
 // In case of error building listeners, an appropriate error is returned,
 // along with partial list of listeners. It is responsibility of the caller to
 // close the returned listeners whenever required.
 //
-// Multiple file descriptors are returned when listening on both IPv4 and
+// Multiple [net.Listener] may be returned when listening on both IPv4 and
 // IPv6. Ensure that your server correctly handles listening on multiple
 // [net.Listener].
 //
@@ -35,21 +32,18 @@ import (
 //
 // [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
 func TCPListeners(name string) ([]net.Listener, error) {
-	fdSlice, err := listenerFdsWithName(name)
+	files, err := Files(name)
 	if err != nil {
 		return nil, err
 	}
 
-	listeners := make([]net.Listener, 0, len(fdSlice))
-	for _, fd := range fdSlice {
-		if fd != 0 {
-			file := os.NewFile(uintptr(fd), fmt.Sprintf("launchd-activate-tcp-socket-%s", name))
-			fl, el := net.FileListener(file)
-			if el != nil {
-				err = errors.Join(err, el)
-			} else {
-				listeners = append(listeners, fl)
-			}
+	listeners := make([]net.Listener, 0, len(files))
+	for _, file := range files {
+		l, el := net.FileListener(file)
+		if el != nil {
+			err = errors.Join(err, el)
+		} else {
+			listeners = append(listeners, l)
 		}
 	}
 
@@ -62,14 +56,11 @@ func TCPListeners(name string) ([]net.Listener, error) {
 // UDPListeners returns slice of [net.PacketConn] for specified UDP
 // socket name, as  mentioned in launchd plist file.
 //
-// This does not make use of cgo and makes calls to using libc directly.
-// This is obviously not supported on iOS.
-//
 // In case of error building listeners, an appropriate error is returned,
 // along with partial list of listeners. It is responsibility of the caller to
 // close the returned listeners whenever required.
 //
-// Multiple file descriptors are returned when listening on both IPv4 and
+// Multiple [net.PacketConn] may be returned when listening on both IPv4 and
 // IPv6. Ensure that your server correctly handles listening on multiple
 // [net.Listener].
 //
@@ -83,21 +74,18 @@ func TCPListeners(name string) ([]net.Listener, error) {
 //
 // [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
 func UDPListeners(name string) ([]net.PacketConn, error) {
-	fdSlice, err := listenerFdsWithName(name)
+	files, err := Files(name)
 	if err != nil {
 		return nil, err
 	}
 
-	listeners := make([]net.PacketConn, 0, len(fdSlice))
-	for _, fd := range fdSlice {
-		if fd != 0 {
-			file := os.NewFile(uintptr(fd), fmt.Sprintf("launchd-activate-udp-socket-%s", name))
-			fl, el := net.FilePacketConn(file)
-			if el != nil {
-				err = errors.Join(err, el)
-			} else {
-				listeners = append(listeners, fl)
-			}
+	listeners := make([]net.PacketConn, 0, len(files))
+	for _, file := range files {
+		l, el := net.FilePacketConn(file)
+		if el != nil {
+			err = errors.Join(err, el)
+		} else {
+			listeners = append(listeners, l)
 		}
 	}
 
@@ -105,4 +93,39 @@ func UDPListeners(name string) ([]net.PacketConn, error) {
 		return slices.Clip(listeners), fmt.Errorf("launchd: %w", err)
 	}
 	return slices.Clip(listeners), nil
+}
+
+// Files returns slice of [*os.File] backed by file descriptor for socket name,
+// as  mentioned in launchd plist file.
+//
+// In case of error building listeners, an appropriate error is returned,
+// along with partial list of listeners. It is responsibility of the caller to
+// close the returned listeners whenever required.
+//
+// Multiple files may be returned when listening on both IPv4 and
+// IPv6. Ensure that your server correctly handles listening on multiple
+// [net.Listener].
+//
+//   - [syscall.EALREADY] is returned if socket is already activated.
+//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket is not
+//     found. According to [Apple Launchd Documentation], expected error is
+//     [syscall.ENOENT], however implementations return [syscall.ESRCH].
+//   - [syscall.ESRCH] is returned if calling process is not manged by launchd.
+//   - [syscall.EINVAL] is returned if name contains null characters.
+//   - [syscall.ENOSYS] is returned on non macOS platforms (including iOS).
+//
+// [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
+func Files(name string) ([]*os.File, error) {
+	fdSlice, err := listenerFdsWithName(name)
+	if err != nil {
+		return nil, err
+	}
+	files := make([]*os.File, 0, len(fdSlice))
+	for _, fd := range fdSlice {
+		if fd != 0 {
+			files = append(files, os.NewFile(uintptr(fd),
+				fmt.Sprintf("launchd-socket-%s", name)))
+		}
+	}
+	return slices.Clip(files), nil
 }
