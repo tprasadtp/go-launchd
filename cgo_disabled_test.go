26d073e8e7b711c727cbb21ed79728164fb3bdfb
Prasad Tengse <tprasadtp@users.noreply.github.com>
2023-11-19 06:28:53 +0100

    feat(test): Add cgo tests to ensure implementation works when using cgo

---
+++ cgo_disabled_test.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright 2023 Prasad Tengse
+// SPDX-License-Identifier: MIT
+
+//go:build !cgo
+
+package launchd_test
+
+import (
+	"testing"
+)
+
+func TestCgoTests(t *testing.T) {
+	t.SkipNow()
+}
