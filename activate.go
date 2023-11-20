fb62d017ad46f6e1c1b46a0b717a0cddfec3b3c1
Prasad Tengse <tprasadtp@users.noreply.github.com>
2023-11-20 01:40:37 +0100

    docs: update docs

--- activate.go
+++ activate.go
@@ -11,26 +11,51 @@ import (
 	"slices"
 )
 
-// TCPListeners returns slice of [net.Listener] for specified TCP socket,
-// as mentioned in launchd plist file.
+// Files returns slice of [*os.File] backed by file descriptor(s) for given socket.
 //
 // In case of error building listeners, an appropriate error is returned,
 // along with partial list of listeners. It is responsibility of the caller to
 // close the returned listeners whenever required.
+//
+// Multiple files may be returned when listening on both IPv4 and IPv6.
+//
+//   - [syscall.EALREADY] is returned if socket is already activated.
+//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket
+//     is not found.
+//   - [syscall.ESRCH] is returned if calling process is not manged by launchd.
+//   - [syscall.EINVAL] is returned if name contains null characters.
+//   - [syscall.ENOSYS] is returned on non macOS platforms (including iOS).
+func Files(name string) ([]*os.File, error) {
+	fdSlice, err := listenerFdsWithName(name)
+	if err != nil {
+		return nil, err
+	}
+	files := make([]*os.File, 0, len(fdSlice))
+	for _, fd := range fdSlice {
+		if fd != 0 {
+			files = append(files, os.NewFile(uintptr(fd),
+				fmt.Sprintf("launchd-socket-%s", name)))
+		}
+	}
+	return slices.Clip(files), nil
+}
+
+// TCPListeners returns slice of [net.Listener] for specified TCP socket.
+//
+// In case of error building listeners, an appropriate error is returned,
+// along with partial list of listeners. It is responsibility of the caller to
+// close the returned returned non nil listeners whenever required.
 //
 // Multiple [net.Listener] may be returned when listening on both IPv4 and
 // IPv6. Ensure that your server correctly handles listening on multiple
 // [net.Listener].
 //
 //   - [syscall.EALREADY] is returned if socket is already activated.
-//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket is not
-//     found. According to [Apple Launchd Documentation], expected error is
-//     [syscall.ENOENT], however implementations return [syscall.ESRCH].
+//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket
+//     is not found.
 //   - [syscall.ESRCH] is returned if calling process is not manged by launchd.
 //   - [syscall.EINVAL] is returned if name contains null characters.
 //   - [syscall.ENOSYS] is returned on non macOS platforms (including iOS).
-//
-// [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
 func TCPListeners(name string) ([]net.Listener, error) {
 	files, err := Files(name)
 	if err != nil {
@@ -53,26 +78,22 @@ func TCPListeners(name string) ([]net.Listener, error) {
 	return slices.Clip(listeners), nil
 }
 
-// UDPListeners returns slice of [net.PacketConn] for specified UDP
-// socket name, as  mentioned in launchd plist file.
+// UDPListeners returns slice of [net.PacketConn] for specified UDP socket.
 //
 // In case of error building listeners, an appropriate error is returned,
 // along with partial list of listeners. It is responsibility of the caller to
-// close the returned listeners whenever required.
+// close the returned non nil listeners whenever required.
 //
 // Multiple [net.PacketConn] may be returned when listening on both IPv4 and
 // IPv6. Ensure that your server correctly handles listening on multiple
-// [net.Listener].
+// [net.PacketConn].
 //
 //   - [syscall.EALREADY] is returned if socket is already activated.
-//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket is not
-//     found. According to [Apple Launchd Documentation], expected error is
-//     [syscall.ENOENT], however implementations return [syscall.ESRCH].
+//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket
+//     is not found.
 //   - [syscall.ESRCH] is returned if calling process is not manged by launchd.
 //   - [syscall.EINVAL] is returned if name contains null characters.
 //   - [syscall.ENOSYS] is returned on non macOS platforms (including iOS).
-//
-// [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
 func UDPListeners(name string) ([]net.PacketConn, error) {
 	files, err := Files(name)
 	if err != nil {
@@ -93,39 +114,4 @@ func UDPListeners(name string) ([]net.PacketConn, error) {
 		return slices.Clip(listeners), fmt.Errorf("launchd: %w", err)
 	}
 	return slices.Clip(listeners), nil
-}
-
-// Files returns slice of [*os.File] backed by file descriptor for socket name,
-// as  mentioned in launchd plist file.
-//
-// In case of error building listeners, an appropriate error is returned,
-// along with partial list of listeners. It is responsibility of the caller to
-// close the returned listeners whenever required.
-//
-// Multiple files may be returned when listening on both IPv4 and
-// IPv6. Ensure that your server correctly handles listening on multiple
-// [net.Listener].
-//
-//   - [syscall.EALREADY] is returned if socket is already activated.
-//   - [syscall.ENOENT] or [syscall.ESRCH] is returned if specified socket is not
-//     found. According to [Apple Launchd Documentation], expected error is
-//     [syscall.ENOENT], however implementations return [syscall.ESRCH].
-//   - [syscall.ESRCH] is returned if calling process is not manged by launchd.
-//   - [syscall.EINVAL] is returned if name contains null characters.
-//   - [syscall.ENOSYS] is returned on non macOS platforms (including iOS).
-//
-// [Apple Launchd Documentation]: https://developer.apple.com/documentation/xpc/1505523-launch_activate_socket
-func Files(name string) ([]*os.File, error) {
-	fdSlice, err := listenerFdsWithName(name)
-	if err != nil {
-		return nil, err
-	}
-	files := make([]*os.File, 0, len(fdSlice))
-	for _, fd := range fdSlice {
-		if fd != 0 {
-			files = append(files, os.NewFile(uintptr(fd),
-				fmt.Sprintf("launchd-socket-%s", name)))
-		}
-	}
-	return slices.Clip(files), nil
 }
