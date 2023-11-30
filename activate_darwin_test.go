9a2351877c2ea5b0655dbac39bd6707493d0a8a4
Prasad Tengse <10941447+tprasadtp@users.noreply.github.com>
2023-11-30 21:25:04 +0100

    test(fix): allow running tests without coverage flag(s) (#19)

--- activate_darwin_test.go
+++ activate_darwin_test.go
@@ -63,6 +63,45 @@ type TemplateData struct {
 //go:embed internal/testdata/launchd.plist
 var plistTemplate string
 
+// TestingCoverDir coverage data directory. Returns empty if coverage is not
+// enabled or if test.gocoverdir flag or GOCOVERDIR env variable is not specified.
+//
+// This uses Undocumented/Unexported test flag: -test.gocoverdir.
+// https://github.com/golang/go/issues/51430#issuecomment-1344711300
+func TestingCoverDir(t *testing.T) string {
+	t.Helper()
+
+	// The return value will be empty if test coverage is not enabled.
+	if testing.CoverMode() != "" {
+		return ""
+	}
+
+	var goCoverDir string
+	var gocoverdirFlag = flag.Lookup("test.gocoverdir")
+	if goCoverDir == "" && gocoverdirFlag != nil {
+		goCoverDir = gocoverdirFlag.Value.String()
+	}
+
+	var goCoverDirEnv = strings.TrimSpace(os.Getenv("GOCOVERDIR"))
+	if goCoverDir == "" && goCoverDirEnv != "" {
+		goCoverDir = goCoverDirEnv
+	}
+
+	// Return empty string
+	if goCoverDir == "" {
+		return ""
+	}
+
+	// Get absolute path for GoCoverDir.
+	// Because launchd unit may run under different working directory.
+	goCoverDirAbs, err := filepath.Abs(goCoverDir)
+	if err != nil {
+		t.Fatalf("Failed to get absolute path of test.gocoverdir(%s):%s",
+			goCoverDir, err)
+	}
+	return goCoverDirAbs
+}
+
 // GetFreePort asks the kernel for a free open port that is ready to use.
 func GetFreePort(t *testing.T) int {
 	t.Helper()
@@ -707,44 +746,13 @@ func TestLaunchd(t *testing.T) {
 	//
 	bundle := fmt.Sprintf("test.go-svc.%s", hex.EncodeToString(rb))
 
-	// Define coverage data directory.
-	//
-	// This uses Undocumented/Unexported test flag: -test.gocoverdir.
-	// https://github.com/golang/go/issues/51430#issuecomment-1344711300
-	var goCoverDir string
-	var goCoverDirEnv = strings.TrimSpace(os.Getenv("GOCOVERDIR"))
-	var gocoverdirFlag = flag.Lookup("test.gocoverdir")
-	if goCoverDir == "" && gocoverdirFlag != nil {
-		goCoverDir = gocoverdirFlag.Value.String()
-		t.Logf("test.gocoverdir=%s(via test-flag)", goCoverDir)
-	}
-
-	if goCoverDir == "" && goCoverDirEnv != "" {
-		goCoverDir = goCoverDirEnv
-		t.Logf("test.gocoverdir=%s(via env GOCOVERDIR)", goCoverDir)
-	}
-
-	// Fallback to per test's temp dir.
-	if goCoverDir == "" {
-		goCoverDir = t.TempDir()
-		t.Logf("test.gocoverdir=%s(testing.TempDir)", goCoverDir)
-	}
-
-	// Get absolute path for GoCoverDir.
-	// Because launchd unit may run under different working directory.
-	goCoverDirAbs, err := filepath.Abs(goCoverDir)
-	if err != nil {
-		t.Fatalf("Failed to get absolute path of test.gocoverdir(%s):%s",
-			goCoverDir, err)
-	}
-
 	plistFileName := filepath.Join(agentsDir, fmt.Sprintf("%s.plist", bundle))
 	data := TemplateData{
 		BundleID:                 bundle,
 		GoTestServerAddr:         server.URL,
 		GoTestBinary:             os.Args[0],
 		GoTestName:               "^(TestRemote|TestTrampoline)",
-		GoCoverDir:               goCoverDirAbs,
+		GoCoverDir:               TestingCoverDir(t),
 		StdoutFile:               stdout,
 		StderrFile:               stderr,
 		TCP:                      strconv.Itoa(GetFreePort(t)),
